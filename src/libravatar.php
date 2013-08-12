a4baae038465f0b45f99993684c8535ee5fe55bc
Christian Archer <chrstnarchr@aol.com>
2013-08-12 19:53:23 +0400

    * Libravatar Replace 2.0 ** Replace Gravatar with Libravatar, not just add extra default ** Beta support for BuddyPress

--- src/libravatar.php
+++ src/libravatar.php
@@ -1,102 +1,107 @@
 <?php
 /*
- Plugin Name: Libravatar
- Plugin URI: http://www.gabsoftware.com/products/scripts/libravatar/
- Description: Libravatar support for Wordpress
- Version: 1.0.3
- Author: Gabriel Hautclocq
- Author URI: http://www.gabsoftware.com/
+ Plugin Name: Libravatar Replace
+ Plugin URI: http://code.sunchaser.info/libravatar
+ Description: Libravatar support for WordPress and BuddyPress
+ Version: 2.0.0
+ Author: Christian Archer, based on work by Gabriel Hautclocq
+ Author URI: https://sunchaser.info/
  License: ISC
 */
 
 //error_reporting( E_ALL );
 
 // security check
-if( !defined( 'WP_PLUGIN_DIR' ) )
+if (!defined('WP_PLUGIN_DIR'))
 {
-	die( 'There is nothing to see here!' );
+	die('There is nothing to see here!');
 }
 
-/* constants */
-// Libravatar text domain
-define( 'LIBRAVATAR_TD', 'libravatar' );
+include_once('Services_Libravatar.php');
 
-// Libravatar version
-define( 'LIBRAVATAR_VERSION_MAJ', 1 );
-define( 'LIBRAVATAR_VERSION_MIN', 0 );
-define( 'LIBRAVATAR_VERSION_REV', 3 );
-
-include_once( 'Services_Libravatar.php' );
-
-add_filter( 'get_avatar', 'libravatar_get_avatar_filter_callback', 10, 5 );
-add_filter( 'avatar_defaults', 'libravatar_avatar_defaults_filter_callback' );
-
-//return true if the connection uses SSL
-function libravatar_is_ssl()
+/**
+ * Class LibravatarReplace
+ *
+ * everything for the plugin to work
+ */
+class LibravatarReplace
 {
-	if( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] != 'off' )
-	{
-		return true;
-	}
-	elseif( $_SERVER['SERVER_PORT'] == 443 )
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
+	var $bp_catched_last_email;
 
-function libravatar_avatar_defaults_filter_callback( $avatar_defaults )
-{
-	$avatar_defaults['libravatar_default']   = __('Libravatar Logo');
-	$avatar_defaults['libravatar_mm']        = __('Libravatar Mystery Man');
-	$avatar_defaults['libravatar_identicon'] = __('Libravatar Identicon');
-	$avatar_defaults['libravatar_monsterid'] = __('Libravatar Monster ID');
-	$avatar_defaults['libravatar_wavatar']   = __('Libravatar Wavatar');
-	$avatar_defaults['libravatar_retro']     = __('Libravatar Retro');
-	return $avatar_defaults;
-}
-
-function libravatar_get_avatar_filter_callback( $avatar, $id_or_email, $size, $default, $alt )
-{
-	if (preg_match('/^libravatar_(.*)$/', $default, $matches))
+	/**
+	 * @return bool true if the connection uses SSL
+	 */
+	function isSsl()
 	{
-		if( false === $alt)
+		if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off')
+		{
+			return true;
+		}
+		elseif ($_SERVER['SERVER_PORT'] == 443)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Change Avatar Defaults
+	 *
+	 * @param array $avatar_defaults
+	 * @return mixed
+	 */
+	function filterAvatarDefaultsCallback($avatar_defaults)
+	{
+		$avatar_defaults['gravatar_default'] = __('Libravatar Logo'); // rename accordingly
+		return $avatar_defaults;
+	}
+
+	/**
+	 * Create avatar link
+	 *
+	 * @param string $avatar
+	 * @param string|int $id_or_email
+	 * @param int $size
+	 * @param string $default
+	 * @param string $alt
+	 * @return string avatar HTML
+	 */
+	function filterGetAvatarCallback($avatar, $id_or_email, $size, $default, $alt)
+	{
+		if (false === $alt)
 		{
 			$safe_alt = '';
 		}
 		else
 		{
-			$safe_alt = esc_attr( $alt );
+			$safe_alt = esc_attr($alt);
 		}
 
 		$email = '';
-		if( is_numeric( $id_or_email ) )
+		if (is_numeric($id_or_email))
 		{
-			$id = (int) $id_or_email;
-			$user = get_userdata( $id );
-			if( $user )
+			$id = (int)$id_or_email;
+			$user = get_userdata($id);
+			if ($user)
 			{
 				$email = $user->user_email;
 			}
 		}
-		elseif( is_object( $id_or_email ) )
+		elseif (is_object($id_or_email))
 		{
-			// No avatar for pingbacks or trackbacks
-			$allowed_comment_types = apply_filters( 'get_avatar_comment_types', array( 'comment' ) );
-
-			if( ! empty( $id_or_email->user_id ) )
+			if (!empty($id_or_email->user_id))
 			{
-				$id = (int) $id_or_email->user_id;
-				$user = get_userdata( $id );
-				if( $user)
+				$id = (int)$id_or_email->user_id;
+				$user = get_userdata($id);
+				if ($user)
 				{
 					$email = $user->user_email;
 				}
 			}
-			elseif( ! empty( $id_or_email->comment_author_email ) )
+			elseif (!empty($id_or_email->comment_author_email))
 			{
 				$email = $id_or_email->comment_author_email;
 			}
@@ -110,19 +115,68 @@ function libravatar_get_avatar_filter_callback( $avatar, $id_or_email, $size, $d
 		$options = array();
 		$options['s'] = $size;
 		$options['algorithm'] = 'md5';
-		$options['https'] = libravatar_is_ssl();
-		if ($matches[1] !== 'default') {
-			$options['d'] = $matches[1];
+		$options['https'] = $this->isSsl();
+		if ($default && $default !== 'gravatar_default')
+		{
+			if ($default === 'blank')
+			{
+				$default = 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=blank&s=' . $size;
+			}
+			$options['d'] = $default;
 		}
-		$url = $libravatar->url( $email, $options );
+		$url = $libravatar->url($email, $options);
 
 		$avatar = "<img alt='{$safe_alt}' src='{$url}' class='avatar avatar-{$size} photo' height='{$size}' width='{$size}' />";
+
+		return $avatar;
 	}
 
-	return $avatar;
+	/**
+	 * There's no way to know email on the url composing so just remember the email in the previous filter
+	 *
+	 * @param $email
+	 * @return mixed
+	 */
+	function filterBPCoreGravatarEmail($email)
+	{
+		$this->bp_catched_last_email = $email;
+
+		return $email;
+	}
+
+	/**
+	 * Shitcode here :)
+	 *
+	 * Take the remembered email and get url from it, then extract the host
+	 * Unfortunately there's no way to set Libravatar link with federation support more direct
+	 *
+	 * @param $host
+	 * @return string
+	 */
+	function filterBPGravatarUrl($host)
+	{
+		$default_host = $this->isSsl() ? 'https://seccdn.libravatar.org/avatar/' : 'http://cdn.libravatar.org/avatar/';
+
+		if (empty($this->bp_catched_last_email))
+		{
+			return $default_host;
+		}
+
+		$libravatar = new Services_Libravatar();
+		$options = array();
+		$options['algorithm'] = 'md5';
+		$options['https'] = $this->isSsl();
+		$url = $libravatar->getUrl($this->bp_catched_last_email, $options);
+
+		preg_match('~^(.*/avatar/)~', $url, $matches);
+
+		return isset($matches[1]) ? $matches[1] : $default_host;
+	}
 }
 
+$libravatar_replace = new LibravatarReplace();
 
-
-
-?>
+add_filter('get_avatar',                array($libravatar_replace, 'filterGetAvatar'), 10, 5);
+add_filter('avatar_defaults',           array($libravatar_replace, 'filterAvatarDefaults'));
+add_filter('bp_core_gravatar_email',    array($libravatar_replace, 'filterBPCoreGravatarEmail'));
+add_filter('bp_gravatar_url',           array($libravatar_replace, 'filterBPGravatarUrl', 10));
--- src/readme.txt
+++ src/readme.txt
@@ -1,21 +1,16 @@
-=== Libravatar ===
-Contributors: GabSoftware
-Donate link: http://www.gabsoftware.com/donate/
-Tags: libravatar, avatar, support, user, email, pseudo, picture, image
-Requires at least: 3.0.0
-Tested up to: 3.2.1
-Stable tag: 1.0.3
+=== Libravatar Replace ===
+Contributors: SunChaser, GabSoftware
+Tags: libravatar, avatar, support, user, email, pseudo, picture, image, buddypress
+Requires at least: 3.6
+Tested up to: 3.6
+Stable tag: 2.0.0
 
-Adds Libravatar support to your WordPress installation.
+Replaces Gravatar with Libravatar in your WordPress installation.
 
 == Description ==
 
 <p>
 Adds <a href="http://www.libravatar.org">Libravatar</a> avatars support to your WordPress installation. Libravatar is an alternative to Gravatar and its API is mostly compatible.
-</p>
-
-<p>
-To use Libravatars, just select the "Libravatar Logo" option in the Discussion panel of your WordPress administration area. Of course you are advised to create an account in Libravatar!
 </p>
 
 == Installation ==
@@ -26,32 +21,22 @@ Just extract the plugin directory into your wp-content/plugins directory.
 
 == Frequently Asked Questions ==
 = What are the requirements of this plugin? =
-Exactly the same as the current stable version of WordPress!
-
-== Screenshots ==
-
-1. The new Libravatar entry in the avatar list
+PHP 5 is required, PHP 5.3 is strongly encouraged
 
 == Thanks to ==
+* Gabriel Hautclocq, author of the initial plugin
 * François Marier (For his hard work in the Libravatar project and his bug fixes)
 
 == Changelog ==
 
+= 2.0.0 =
+(Differences from the Libravatar plugin)
+* Updated Services_Libravatar with SRV port discovery fix
+* The plugin tries to completely replace Libravatar with Gravatar
+* Beta support for BuddyPress
+
 = 1.0.3 =
-* Oops, I forgot to add Services_Libravatar.php before! Should be fixed now.
-
-= 1.0.2 =
-* Support for federation (self hosted avatars functionnality of Libravatar)
-* Now uses the Services_Libravatar class from Libravatar
-
-= 1.0.1 =
-* Corrected invalid s query arg
-* Corrected default picture
-* Corrected the version constants
-* Corrected the (wrong) text domain
-
-= 1.0.0 =
-* Initial release
+* Initial fork of the Libravatar plugin
 
 == Upgrade Notice ==
 
