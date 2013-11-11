b98e50aea2ff9bd56eb984d7ffc10710be572626
Christian Archer <chrstnarchr@aol.com>
2013-11-12 02:45:00 +0400

    build.php is apache licensed

--- build/build.php
+++ build/build.php
@@ -6,6 +6,18 @@
  * Build system for the Libravatar Replace WordPress plugin
  * Copyright © 2013, Christian Archer <chrstnarchr@aol.com>
  *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
  * Here's the dilemma:
  * 1) It's a bad idea to store external libraries in your VCS
  * 2) All files should be included into the WordPress Plugin SVN
