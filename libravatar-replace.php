2166398a22dadf94c523a414fa6d18ecf4a7ebf9
Christian Archer <chrstnarchr@aol.com>
2013-11-12 02:35:13 +0400

    whoops, ISC _is_ GPL compatible, remove dual licensing

--- libravatar-replace.php
+++ libravatar-replace.php
@@ -7,7 +7,7 @@
  * Version: 2.0.4
  * Author: Christian Archer
  * Author URI: https://sunchaser.info/
- * License: ISC, GNU GPL 2.0+
+ * License: ISC
  * Initial Author: Gabriel Hautclocq
  * Initial Author URI: http://www.gabsoftware.com/
  */
