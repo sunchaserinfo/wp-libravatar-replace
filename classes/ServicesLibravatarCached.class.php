e5a98e42e5d5e3be05241d5e73d1d5fc88a10484
Christian Archer <chrstnarchr@aol.com>
2013-12-13 00:36:24 +0400

    3.0 feature: allow plugin to download avatars instead of showing remote images
    --HG--
    branch : 3.0-dev

---
+++ classes/ServicesLibravatarCached.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class ServicesLibravatarCached extends ServicesLibravatarExt
+{
+    public function getUrl($identifier, $options = array())
+    {
+        // todo: download image and store it for 3 days, show cached image
+    }
+}
