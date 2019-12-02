a1f011823881664e2dc27060b694b6b5c64df5fe
Christian Archer <sunchaser@sunchaser.info>
2019-12-03 00:29:22 +0200

    Remove languages

--- Pakefile.php
+++ Pakefile.php
@@ -8,24 +8,6 @@ function run_readme()
     pake_sh('vendor/bin/wp2md -i readme.txt -o README.md');
 }
 
-pake_desc('Build language files');
-pake_task('lang');
-
-function run_lang()
-{
-    $dir = new DirectoryIterator(__DIR__ . '/languages');
-
-    foreach ($dir as $file) {
-        /** @var $file DirectoryIterator */
-        if ($file->getExtension() === 'po') {
-            $po_file = escapeshellarg($file->getPathname());
-            $mo_file = escapeshellarg($file->getPath() . '/' . $file->getBasename('.po') . '.mo');
-
-            pake_sh("msgfmt $po_file -o $mo_file");
-        }
-    }
-}
-
 pake_desc('Build project for WordPress SVN');
 pake_task('build');
 
@@ -37,7 +19,6 @@ function run_build()
     $classes_dir        = $base_dir . '/classes';
     $cache_dir          = $base_dir . '/cache';
     $views_dir          = $base_dir . '/views';
-    $lang_dir           = $base_dir . '/languages';
 
     $svn_dir            = $base_dir . '/build/svn';
     $svn_trunk_dir      = $svn_dir . '/trunk';
@@ -46,16 +27,12 @@ function run_build()
     $svn_classes_dir    = $svn_trunk_dir . '/classes';
     $svn_cache_dir      = $svn_trunk_dir . '/cache';
     $svn_views_dir      = $svn_trunk_dir . '/views';
-    $svn_lang_dir       = $svn_trunk_dir . '/languages';
 
     pake_echo('Prepare trunk and assets dirs');
     pake_mkdirs($svn_trunk_dir);
     pake_mkdirs($svn_assets_dir);
     pake_remove(pakeFinder::type('any'), $svn_trunk_dir);
     pake_remove(pakeFinder::type('any'), $svn_assets_dir);
-
-    pake_echo('Build languages');
-    run_lang();
 
     pake_echo('Copy assets');
     pake_mirror(pakeFinder::type('file'), $assets_dir, $svn_assets_dir);
@@ -70,7 +47,6 @@ function run_build()
     pake_mirror(pakeFinder::type('file'), $classes_dir, $svn_classes_dir);
     pake_mirror(pakeFinder::type('file'), $cache_dir,   $svn_cache_dir);
     pake_mirror(pakeFinder::type('file'), $views_dir,   $svn_views_dir);
-    pake_mirror(pakeFinder::type('file'), $lang_dir,    $svn_lang_dir);
 
     pake_echo('Retrieve Services_Libravatar class');
     $loader = require($base_dir . '/vendor/autoload.php');
